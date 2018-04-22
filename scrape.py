aecb9915a5c371b0a3c8274d48ed71d1abaf54f6
pseute <patrick.seute@gmail.com>
2018-04-21 21:26:53 -0400

    initial commit

---
+++ scrape.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+
+my_url = "http://www.espn.com/nfl/playbyplay?gameId=400951580"
+
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+page_soup = soup(page_html, "html.parser")
+containers = page_soup.findAll("td", {"class": "game-details"})
+
+scoring_plays = []
+
+
+for container in containers:
+    new_score = {}
+    new_score["type"] = container.findAll("div", {"class": "score-type"})[0].text
+    headline = container.findAll("div", {"class": "headline"})[0].text
+    new_score["player"] = re.search('^\D+').group(0).strip
+    new_score["yards"] = re.search('\d+\sYd', headline).group(0).split()[0]
+
+    print(new_score)
