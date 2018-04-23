5e16b0ac9d43ae9d21e10423bb2ec40cedc3c3f6
pseute <patrick.seute@gmail.com>
2018-04-23 19:36:08 -0400

    make all quotes single quotes for consistency

--- scrape.py
+++ scrape.py
@@ -6,30 +6,30 @@ import json
 
 def get_match_containers(gameId):
     #connecting to client and downloading page information
-    match_url = "http://www.espn.com/nfl/game?gameId=" + str(gameId)
+    match_url = 'http://www.espn.com/nfl/game?gameId=' + str(gameId)
     uClient = uReq(match_url)
     page_html = uClient.read()
     uClient.close()
-    page_soup = soup(page_html, "html.parser")
-    containers = page_soup.findAll("td", {"class": "game-details"})
+    page_soup = soup(page_html, 'html.parser')
+    containers = page_soup.findAll('td', {'class': 'game-details'})
     return containers
 
 
 #prints the cores in a readable way for testing purposes
 def print_scores(scores):
     for score in scores:
-        if score["type"] == "FG":
-            print("{} by {} for {} Yards".format(score["type"], score["player"], score["yards"]))
-        elif score["play_type"] == "run":
-            print("{} run by {} for {} Yards".format(score["type"], score["player"], score["yards"]))
-        elif score["play_type"] == "pass":
-            print("{} pass from {} to {} for {} Yards".format(score["type"], score["passer"], score["player"], score["yards"]))
-        elif score["type"] == "PAT":
-            print("{} good by {}".format(score["type"], score["player"]))
-        elif score["type"] == "SF":
-            print("Safety by {}".format(score["player"]))
+        if score['type'] == 'FG':
+            print('{} by {} for {} Yards'.format(score['type'], score['player'], score['yards']))
+        elif score['play_type'] == 'run':
+            print('{} run by {} for {} Yards'.format(score['type'], score['player'], score['yards']))
+        elif score['play_type'] == 'pass':
+            print('{} pass from {} to {} for {} Yards'.format(score['type'], score['passer'], score['player'], score['yards']))
+        elif score['type'] == 'PAT':
+            print('{} good by {}'.format(score['type'], score['player']))
+        elif score['type'] == 'SF':
+            print('Safety by {}'.format(score['player']))
         else:
-            print("{} by {} for {} Yards".format(score["type"], score["player"], score["yards"]))
+            print('{} by {} for {} Yards'.format(score['type'], score['player'], score['yards']))
 
 
 #retrieves all the scoring plays from a single match
@@ -47,32 +47,32 @@ def parse_play(container):
     scores = []
     new_score = {}
     no_kick = False
-    new_score["type"] = container.findAll("div", {"class": "score-type"})[0].text
-    headline = container.findAll("div", {"class": "headline"})[0].text
-    if headline[-1] != ")":
+    new_score['type'] = container.findAll('div', {'class': 'score-type'})[0].text
+    headline = container.findAll('div', {'class': 'headline'})[0].text
+    if headline[-1] != ')':
         no_kick = True
-    if new_score["type"] != "SF":
-        new_score["player"] = re.search('^\D+', headline).group(0).strip()
-        new_score["yards"] = re.search('\d+\sYd', headline).group(0).split()[0]
+    if new_score['type'] != 'SF':
+        new_score['player'] = re.search('^\D+', headline).group(0).strip()
+        new_score['yards'] = re.search('\d+\sYd', headline).group(0).split()[0]
     else:
-        new_score["player"] = re.search('\D+?(?=\sSafety)',headline).group(0)
-        new_score["yards"] = "NA"
+        new_score['player'] = re.search('\D+?(?=\sSafety)',headline).group(0)
+        new_score['yards'] = 'NA'
     #if the score was a touchdown, extract extra information
     if new_score['type'] == 'TD':
         #gets whether the TD was a run/pass
         if not no_kick:
-            new_score["play_type"] = re.search('Yd\s(\w*)\s', headline).group(1).lower()
+            new_score['play_type'] = re.search('Yd\s(\w*)\s', headline).group(1).lower()
         else:
-            new_score["play_type"] = re.search('Yd\s(\w*)$', headline).group(1).lower()
+            new_score['play_type'] = re.search('Yd\s(\w*)$', headline).group(1).lower()
         #if the touchdown was a pass, get the passer information
-        if new_score["play_type"] == "pass":
+        if new_score['play_type'] == 'pass':
             #extract passer information
             if not no_kick:
-                new_score["passer"] = re.search('from\s(\D*)\s\(', headline).group(1)
+                new_score['passer'] = re.search('from\s(\D*)\s\(', headline).group(1)
             else:
-                new_score["passer"] = re.search('from\s(\D*)$', headline).group(1)
+                new_score['passer'] = re.search('from\s(\D*)$', headline).group(1)
         else:
-            new_score["passer"] = "NA"
+            new_score['passer'] = 'NA'
         if not no_kick:
             _ , kick = headline.split('(')
             kick = kick.strip(')')
@@ -80,15 +80,15 @@ def parse_play(container):
             #If the point after attempt is successful, record the kicker that scored as a PAT type
             if result == 'Kick':
                 kick_score = {}
-                kick_score["passer"] = "NA"
-                kick_score["type"] = 'PAT'
-                kick_score["player"] = kicker
-                kick_score["yards"] = 'NA'
-                kick_score["play_type"] = "PAT"
+                kick_score['passer'] = 'NA'
+                kick_score['type'] = 'PAT'
+                kick_score['player'] = kicker
+                kick_score['yards'] = 'NA'
+                kick_score['play_type'] = 'PAT'
                 scores.append(kick_score)
     else:
-        new_score["play_type"] = new_score["type"]
-        new_score["passer"] = "NA"
+        new_score['play_type'] = new_score['type']
+        new_score['passer'] = 'NA'
     scores.append(new_score)
     return scores
 
@@ -101,5 +101,5 @@ def get_match_scores(gameId):
 scores = get_match_scores(400951597)
 #print found scores to console and write to .json file
 print_scores(scores)
-with open("stats.json", "w") as writeJSON:
+with open('stats.json', 'w') as writeJSON:
     json.dump(scores, writeJSON, sort_keys=True, indent=4)
