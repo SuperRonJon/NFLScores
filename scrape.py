d75e59a87f3a003a3ddd1ec901875cefe0f8f956
pseute <patrick.seute@gmail.com>
2018-04-22 20:17:40 -0400

    extract passer information if touchdown was a pass

--- scrape.py
+++ scrape.py
@@ -19,7 +19,16 @@ scoring_plays = []
 #prints the cores in a readable way for testing purposes
 def print_scores(scores):
     for score in scores:
-        print("{} by {} for {} Yards".format(score["type"], score["player"], score["yards"]))
+        if score["type"] == "FG":
+            print("{} by {} for {} Yards".format(score["type"], score["player"], score["yards"]))
+        elif score["play_type"] == "run":
+            print("{} run by {} for {} Yards".format(score["type"], score["player"], score["yards"]))
+        elif score["play_type"] == "pass":
+            print("{} pass from {} to {} for {} Yards".format(score["type"], score["passer"], score["player"], score["yards"]))
+        elif score["type"] == "PAT":
+            print("{} good by {}".format(score["type"], score["player"]))
+        else:
+            print("{} by {} for {} Yards".format(score["type"], score["player"], score["yards"]))
 
 #loops through each score and extracts the data
 for container in containers:
@@ -28,8 +37,16 @@ for container in containers:
     headline = container.findAll("div", {"class": "headline"})[0].text
     new_score["player"] = re.search('^\D+', headline).group(0).strip()
     new_score["yards"] = re.search('\d+\sYd', headline).group(0).split()[0]
-    #if the score was a touchdown, extract the Point After Attempt information
+    #if the score was a touchdown, extract extra information
     if new_score['type'] == 'TD':
+        #gets whether the TD was a run/pass
+        new_score["play_type"] = re.search('Yd\s(\w*)\s', headline).group(1).lower()
+        #if the touchdown was a pass, get the passer information
+        if new_score["play_type"] == "pass":
+            #extract passer information
+            new_score["passer"] = re.search('from\s(\D*)\s\(', headline).group(1)
+        else:
+            new_score["passer"] = "NA"
         _ , kick = headline.split('(')
         kick = kick.strip(')')
         kicker, result = kick.rsplit(' ', 1)
@@ -39,7 +56,10 @@ for container in containers:
             kick_score["type"] = 'PAT'
             kick_score["player"] = kicker
             kick_score["yards"] = 'NA'
+            kick_score["play_type"] = "PAT"
             scoring_plays.append(kick_score)
+    else:
+        new_score["play_type"] = new_score["type"]
     scoring_plays.append(new_score)
 
 #print found scores to console and write to .json file
