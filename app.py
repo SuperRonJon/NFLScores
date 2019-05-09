6db44414b53ccc2d1e1a654b48a3643646b8d610
pseute <patrick.seute@gmail.com>
2019-05-08 20:40:00 -0400

    Add CORS access

--- app.py
+++ app.py
@@ -10,11 +10,15 @@ def index():
 @app.route('/scores/<year>/<week>')
 def week_scores(year, week):
     scores = nfl.get_week_scores(year, week)
-    return jsonify(scores)
+    response = jsonify(scores)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 @app.route('/scores/<year>/<week>/<team>')
 def team_week(year, week, team):
     all_scores = nfl.get_week_scores(year, week)
     team_scores = [score for score in all_scores if score['team'] == team]
-    return jsonify(team_scores)
+    response = jsonify(team_scores)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
