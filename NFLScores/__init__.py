ebe110a5a7b9d625bca50cf1d9afb450cdbfc385
Patrick Seute <patrick.seute@cox.net>
2019-05-18 14:33:29 -0400

    Add database support

--- NFLScores/__init__.py
+++ NFLScores/__init__.py
@@ -1,2 +1,2 @@
-from .match_parsing import get_week_scores
+from .match_parsing import get_week_scores, get_week_info, get_game_info
 from .output import write_to_csv, write_to_json, print_scores
--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
 import NFLScores as nfl
 
 app = Flask(__name__)
+
+client = MongoClient('mongodb://localhost')
+db = client.nfls
+
+@app.route('/add', methods=["POST"])
+def add_user():
+    to_add = {'name': request.form['name'], 'phone': request.form['phone']}
+    print(f'Adding {request.form["name"]}, {request.form["phone"]}')
+    db.collections.insert_one(to_add)
+    return 'ok'
 
 @app.route('/')
 def index():
@@ -9,10 +20,21 @@ def index():
 
 @app.route('/scores/<year>/<week>')
 def week_scores(year, week):
-    scores = nfl.get_week_scores(year, week)
-    response = jsonify(scores)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    query = {'week': week, 'year': year}
+    if db.weekdata.count_documents({'week': week, 'year': year}) == 0:
+        print('from scrape')
+        info = nfl.get_week_info(year, week)
+        db.weekdata.insert_one(info)
+        response = jsonify(info['games'])
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    else:
+        print('from db')
+        week_data = db.weekdata.find(query, {'games': True})
+        response = jsonify(week_data[0]['games'])
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
 
 
 @app.route('/scores/<year>/<week>/<team>')
