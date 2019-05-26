7e2bdaefefe54afcf3d32782271a0a108d23237f
Patrick Seute <patrick.seute@cox.net>
2019-05-26 17:11:39 -0400

    Add play to string parsing

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import re
+
 from flask import Flask, jsonify, request, render_template
 from pymongo import MongoClient
 import NFLScores as nfl
@@ -20,7 +22,7 @@ def week_scores():
     if db.weekdata.count_documents(query) == 0:
         info = nfl.get_week_info(year, week)
         db.weekdata.insert_one(info)
-        response = jsonify(info['games'])
+        games = info['games']
     else:
         week_data = db.weekdata.find(query, {'games': True})
         games = week_data[0]['games']
@@ -33,27 +35,46 @@ def game_scores():
     gameid = request.args['id']
     query = {'game_id': gameid}
     if db.gamedata.count_documents(query) == 0:
-        games = dict()
-        games['game_id'] = gameid
-        games['scores'] = nfl.get_match_scores(gameid)
-        games['info'] = nfl.get_match_info(gameid)
-        db.gamedata.insert_one(games)
+        game = dict()
+        game['game_id'] = gameid
+        game['scores'] = nfl.get_match_scores(gameid)
+        game['info'] = nfl.get_match_info(gameid)
+        db.gamedata.insert_one(game)
     else:
         game_data = db.gamedata.find(query)[0]
-        games = {'game_id': game_data['game_id'], 'scores': game_data['scores'], 'info': game_data['info']}
+        game = {'game_id': game_data['game_id'], 'scores': game_data['scores'], 'info': game_data['info']}
+    
+    plays = list()
+    for play in game['scores']:
+        plays.append(make_string(play))
+    game['scores'] = plays
+    print(game)
 
-    return render_template('match.html', scores=games)
+    return render_template('match.html', game=game)
 
 
-
-
-@app.route('/scores/<year>/<week>/<team>')
-def team_week(year, week, team):
-    all_scores = nfl.get_week_scores(year, week)
-    team_scores = [score for score in all_scores if score['team'] == team]
-    response = jsonify(team_scores)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
+def make_string(play):
+    result = ''
+    score = re.search(r'\d+\-\d+', play['score']).group(0)
+    result += '({}) '.format(play['team'].upper())
+    if play['type'] == 'TD':
+        if play['play_type'] == 'pass':
+            result += 'Pass from {} to {} for {} yards'.format(play['passer'], play['player'], play['yards'])
+        elif play['play_type'] == 'run':
+            result += 'Run by {} for {} yards'.format(play['player'], play['yards'])
+        else:
+            result += '{} by {} for {} yards'.format(play['play_type'], play['player'], play['yards'])
+    elif play['type'] == 'PAT':
+        result += 'Point after good by {}'.format(play['player'])
+    elif play['type'] == 'FG':
+        result += 'Field goal good by {} for {} yards'.format(play['player'], play['yards'])
+    elif play['type'] == '2PtConv':
+        if play['play_type'] == 'pass':
+            result += 'Two point conversion pass from {} to {}'.format(play['passer'], play['player'])
+        elif play['play_type'] == 'run':
+            result += 'Two point conversion run by {}'.format(play['player'])
+    elif play['type'] == 'SF':
+        result += 'Safety by {}'.format(play['player'])
+    return result + " " + score
 
 app.run()
